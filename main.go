7378f4d6ac1f587d6f2920a4a6f3cf014ac67b29
alexander <shootnix@gmail.com>
2020-05-21 03:18:12 +0600

    add bandit-server

--- main.go
+++ main.go
@@ -1,32 +1,60 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"log"
+)
+
+type Rec struct {
+	Title       string
+	Description string
+	Thumb       string
+	Type        int64
+	URL         string
+	AmountFrom  float64
+	AmountTo    float64
+	Currency    string
+}
 
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/v1/ping", GETPingV1)
-	router.GET("/v1/feed", GETFeedV1)
-	router.GET("/v1/click", GETClickV1)
+	router.GET("/v1/recs", recsV1)
+	router.GET("/v1/click", clickV1)
 
 	router.Run()
 }
 
+// Взять у бандита рекомендации,
+// соорудить из этого ответ в жсоне:
+func recsV1(c *gin.Context) {
+	resp, err := http.Get("http://localhost:4000/recs/300")
+	if err != nil {
+		c.JSON(500, gin.H{
+			"message": "error",
+		})
+		return
+	}
+	defer resp.Body.Close()
 
-func GETPingV1(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
-}
+	for true {
+        bs := make([]byte, 1014)
+        n, err := resp.Body.Read(bs)
+        log.Println("message from bandit: " + string(bs[:n]))
 
-func GETFeedV1(c *gin.Context) {
+        if n == 0 || err != nil{
+            break
+        }
+    }
+
 	c.JSON(200, gin.H{
 		"message": "feed!",
 	})
 }
 
-func GETClickV1(c *gin.Context) {
+func clickV1(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "click",
 	})
